305ecdd022a91de80e48cce90bac7e48b837c904
Dumitru <dima50066@gmail.com>
2024-10-15 12:28:12 +0200

    add login and register pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,15 @@
-// src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
+import LoginPage from './pages/login/Login';
+import RegisterPage from './pages/register/Register';
 import ResetPassword from './components/auth/ResetPassword';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/register" element={<RegisterPage/>} />
         <Route path="/reset-password" element={<ResetPassword/>} />
       </Routes>
     </Router>
