7ed40114c8eb5273283ead69044455f6b85471be
Dumitru <dima50066@gmail.com>
2024-10-22 13:19:06 +0200

    work with api operations

--- client/src/redux/contacts/operations.ts
+++ client/src/redux/contacts/operations.ts
@@ -1,33 +1,17 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ContactType } from '../../types';
-import axios from 'axios';
-
-const API_URL = 'http://localhost:3001/contacts';
-
-// Налаштування axios
-const api = axios.create({
-  baseURL: API_URL,
-});
-
-// Додаємо обробник запитів для включення токена в заголовки
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+import axiosInstance from '../../api/axiosconfig';
 
 // Функції для роботи з контактами
 export const fetchContacts = createAsyncThunk<ContactType[]>('contacts/fetchContacts', async () => {
-  const response = await api.get('/');
+  const response = await axiosInstance.get('/contacts');
   return response.data.data.data; // Повертаємо дані
 });
 
 export const fetchContactById = createAsyncThunk<ContactType, string>(
   'contacts/fetchContactById',
   async (id) => {
-    const response = await api.get(`/${id}`);
+    const response = await axiosInstance.get(`/contacts/${id}`);
     return response.data; // Повертаємо дані
   }
 );
@@ -35,7 +19,7 @@ export const fetchContactById = createAsyncThunk<ContactType, string>(
 export const createContact = createAsyncThunk<ContactType, Omit<ContactType, '_id'>>(
   'contacts/createContact',
   async (contact) => {
-    const response = await api.post('/', contact);
+    const response = await axiosInstance.post('/contacts', contact);
     return response.data; // Повертаємо створений контакт
   }
 );
@@ -43,7 +27,7 @@ export const createContact = createAsyncThunk<ContactType, Omit<ContactType, '_i
 export const updateContact = createAsyncThunk<ContactType, { id: string; updates: FormData }>(
   'contacts/updateContact',
   async ({ id, updates }) => {
-    const response = await api.put(`/${id}`, updates, {
+    const response = await axiosInstance.put(`/contacts/${id}`, updates, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
@@ -55,7 +39,7 @@ export const updateContact = createAsyncThunk<ContactType, { id: string; updates
 export const deleteContact = createAsyncThunk<string, string>(
   'contacts/deleteContact',
   async (id) => {
-    await api.delete(`/${id}`);
+    await axiosInstance.delete(`/contacts/${id}`);
     return id; // Повертаємо id для видалення контакту
   }
 );
