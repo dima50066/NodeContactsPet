7cd5e0e7a86e7a2da791b53d4d888dec0f02db33
Dumitru <dima50066@gmail.com>
2024-10-22 12:42:48 +0200

    remove usefullcode

--- client/src/pages/contact/ContactPage.tsx
+++ client/src/pages/contact/ContactPage.tsx
@@ -7,32 +7,23 @@ import ContactList from '../../components/contactList/ContactList';
 import ContactForm from '../../components/contactForm/ContactForm';
 import UpdateContactForm from '../../components/updateContactForm/UpdateContactForm';
 import { AppDispatch } from '../../redux/store';
-import { selectIsLoggedIn } from '../../redux/auth/selectors';
-import { useNavigate } from 'react-router-dom';
 
 const ContactPage = () => {
   const dispatch = useDispatch<AppDispatch>();
   const contacts = useSelector(selectContacts);
   const [selectedContact, setSelectedContact] = useState<ContactType | null>(null);
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-  const navigate = useNavigate();
 
   useEffect(() => {
-    if (!isLoggedIn) {
-      navigate('/login');
-    }
-  }, [isLoggedIn, navigate]);
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
-  useEffect(() => {
-    if (isLoggedIn) {
-      dispatch(fetchContacts());
-    }
-  }, [isLoggedIn, dispatch]);
+  const handleAddContact = (newContact: ContactType) => {
+  };
 
   return (
     <div>
       <h1>Contact Page</h1>
-      <ContactForm contacts={contacts} onAdd={(newContact) => {}} />
+      <ContactForm contacts={contacts} onAdd={handleAddContact} />
       <ContactList onSelectContact={setSelectedContact} />
       {selectedContact && (
         <UpdateContactForm
