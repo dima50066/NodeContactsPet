782d6ab09a39555b69c804ae70182fca8903208f
Dumitru <dima50066@gmail.com>
2025-02-10 16:41:33 +0100

    change path

--- client/src/hooks/axiosConfig.ts
+++ client/src/hooks/axiosConfig.ts
@@ -1,31 +1,32 @@
 import axios from 'axios';
 import { store } from '../redux/store';
 
-const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://nodecontactspet-production.up.railway.app'
-  : 'http://localhost:3001';
+const API_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://nodecontactspet.onrender.com/'
+    : 'http://localhost:3001';
 
 const axiosInstance = axios.create({
-    baseURL: API_URL,
+  baseURL: API_URL,
 });
 
-export const setAuthHeader = (token : string) => {
-    axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+export const setAuthHeader = (token: string) => {
+  axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 };
 
 export const clearAuthHeader = () => {
-    delete axiosInstance.defaults.headers.common['Authorization'];
+  delete axiosInstance.defaults.headers.common['Authorization'];
 };
 
 axiosInstance.interceptors.request.use(
-    (config) => {
-        const token = store.getState().auth.token;
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
+  (config) => {
+    const token = store.getState().auth.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
 );
 
 export default axiosInstance;
